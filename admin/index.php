48c1d35325d35b677622e2b519380d08c5262b26
Sonali <programmergithub789@gmail.com>
2023-03-28 19:08:24 +0530

    Update folders

---
+++ admin/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin login pannel</title>
+
+        <?php require('partials/links.php'); ?>
+        <style>
+
+
+
+        </style>
+    </head>
+
+    <body class="bg-light">
+        <div>
+            <form action="">
+                <h4>Admin login pannel </h4>
+                <div>
+                    <div class="mb-3">
+
+                        <input type="text" class="form-control shadow-none text-center " placeholder="Admin name">
+                    </div>
+                    <div class="mb-4">
+
+                        <input type="password" class="form-control shadow-none text-center " placeholder="password">
+                    </div>
+                    <button type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
+                </div>
+            </form>
+        </div>
+
+    </body>
+
+</html>
---
+++ admin/partials/links.php
@@ -0,0 +1,14 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<link
+    href="https://fonts.googleapis.com/css2?family=Edu+NSW+ACT+Foundation:wght@400;500;600&family=Farsan&family=Merienda:wght@400;700;800&family=Merriweather+Sans:ital,wght@1,600;1,700&family=Poppins:wght@400;500;600&display=swap"
+    rel="stylesheet">
+
+
+
+<script src="https://kit.fontawesome.com/7d590b86ec.js" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="CSS/style.css">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
